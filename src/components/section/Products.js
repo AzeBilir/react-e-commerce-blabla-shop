257241ff78d558081f601f1ba3edbc2eb1986e0f
Aze Bilir <azebiliraze@gmail.com>
2021-10-04 13:59:52 +0300

    changes

--- src/components/section/Products.js
+++ src/components/section/Products.js
@@ -4,6 +4,7 @@ import { DataContext } from "../Context";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
+import { Row, Col } from "react-bootstrap";
 import "../css/Products.css";
 
 export class Products extends Component {
@@ -12,35 +13,43 @@ export class Products extends Component {
   render() {
     const { products, addCart } = this.context;
     return (
-      <div id="product">
-        {products.map((product) => (
-          <Box>
-            <Container maxWidth="lg" style={{ marginTop: "10px" }}>
-              <Grid container spacing={3}>
-                <Grid item xs={12} sm={3}>
-                  <div className="card" key={product._id}>
-                    <Link to={`/product/${product._id}`}>
-                      <img src={product.src} alt="" />
-                    </Link>
-                    <div className="content">
-                      <h3>
-                        <Link to={`/product/${product._id}`}>
-                          {product.title}
-                        </Link>
-                      </h3>
-                      <span>${product.price}</span>
-                      <p>{product.description}</p>
-                      <button onClick={() => addCart(product._id)}>
-                        Add to cart
-                      </button>
-                    </div>
-                  </div>
-                </Grid>
-              </Grid>
-            </Container>
-          </Box>
-        ))}
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <div id="product">
+              {products.map((product) => (
+                <Box>
+                  <Grid>
+                    <Container maxWidth="lg" style={{ marginTop: "10px" }}>
+                      <Grid container spacing={3}>
+                        <Grid item xs={12} sm={3}>
+                          <div className="card" key={product._id}>
+                            <Link to={`/product/${product._id}`}>
+                              <img src={product.src} alt="" />
+                            </Link>
+                            <div className="content">
+                              <h4>
+                                <Link to={`/product/${product._id}`}>
+                                  {product.title}
+                                </Link>
+                              </h4>
+                              <span>${product.price}</span>
+                              <p>{product.description}</p>
+                              <button onClick={() => addCart(product._id)}>
+                                Add to cart
+                              </button>
+                            </div>
+                          </div>
+                        </Grid>
+                      </Grid>
+                    </Container>
+                  </Grid>
+                </Box>
+              ))}
+            </div>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
